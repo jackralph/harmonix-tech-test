32cada231b4d214b2d66f924a2c938bd50c9e8b7
Jack Ralph <jackralph@Jacks-MBP.local>
2020-09-06 13:14:48 +0100

    data can be retrieved from database

--- test/mongodb.test.js
+++ test/mongodb.test.js
@@ -1,26 +1,32 @@
-const mongoose = require('mongoose');
-const assert = require('assert');
-const WordComparison = require('../models/WordComparison.model');
+const mongoose = require("mongoose");
+const assert = require("assert");
+const WordComparison = require("../models/WordComparison.model");
 
-describe('mongodb functionality', () => {
-  before(() => {
-    mongoose.connect('mongodb://localhost/word_comparison', {
+describe("mongodb functionality", () => {
+  beforeEach(() => {
+    mongoose.connect("mongodb://localhost/wordcomparison", {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
 
     mongoose.connection
-      .once('open', () => {
-        console.log('connected to db');
+      .once("open", () => {
+        console.log("connected to db");
       })
-      .on('error', (error) => {
+      .on("error", (error) => {
         console.log(error);
       });
   });
-  it('saves a record to the database', (done) => {
+  afterEach((done) => {
+    mongoose.connection.collections.wordcomparisons.drop(() => {
+      console.log("collection dropped");
+      done();
+    });
+  });
+  it("saves a record to the database", (done) => {
     const newWordComparison = new WordComparison({
-      firstWord: 'hello',
-      secondWord: 'world',
+      firstWord: "hello",
+      secondWord: "world",
       anagram: false,
       firstWordPalindrome: false,
       secondWordPalindrome: false,
@@ -31,5 +37,33 @@ describe('mongodb functionality', () => {
       assert(!newWordComparison.isNew);
       done();
     });
+  });
+  it("doesn't save a record if missing required fields", (done) => {
+    const newWordComparisonA = new WordComparison({
+      firstWord: "hello",
+      secondWord: "",
+      anagram: false,
+      firstWordPalindrome: false,
+      secondWordPalindrome: false,
+      timeToComplete: 2,
+    });
+
+    const newWordComparisonB = new WordComparison({
+      firstWord: "hello",
+      secondWord: "",
+      anagram: false,
+      firstWordPalindrome: false,
+      secondWordPalindrome: false,
+      timeToComplete: 2,
+    });
+
+    mongoose.connection.collections.wordcomparisons
+      .insertMany([newWordComparisonA, newWordComparisonB])
+      .then(() => {
+        WordComparison.find().then((documents) => {
+          assert.equal(documents.length, 2);
+          done();
+        });
+      });
   });
 });
